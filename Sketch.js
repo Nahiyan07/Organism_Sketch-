c5b72843c6ffb3c800e56c536012886fca2a0c38
Nahiyan07 <munshinahiyan12@gmail.com>
2026-05-21 17:15:52 +0600

    Initial commit

---
+++ Sketch.js
@@ -0,0 +1,40 @@
+let t = 0;
+let scaleFactor = 2;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  stroke(255, 120);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function organism(x, y) {
+  let k = 5 * cos(x / 14) * cos(y / 30);
+  let e = y / 8 - 13;
+
+  let d = pow(mag(k, e), 2) / 59 + 4;
+
+  let angleTerm = atan2(k, e);
+  let q = 60 - 3 * sin(angleTerm * e);
+
+  let wave = k * (3 + 4 / d * sin(d * d - t * 2));
+
+  let c = d / 2 + e / 99 - t / 18;
+
+  let xCoord = ((q + wave) * sin(c)) * scaleFactor + width / 2;
+  let yCoord = ((q + d * 9) * cos(c)) * scaleFactor + height / 2;
+
+  point(xCoord, yCoord);
+}
+
+function draw() {
+  background(9);
+  t += PI / 20;
+
+  for (let i = 0; i < 10000; i++) {
+    let x = i % 80;
+    let y = i / 43;
+    organism(x, y);
+  }
+}
